266146eff34a559ca28b2b1bc99bbb6b233dc359
Raff <rafiazhim.ra@gmail.com>
2023-11-27 11:59:26 +0700

    update(mainlink.js): add api login

--- assets/js/template/mainlink.js
+++ assets/js/template/mainlink.js
@@ -1,2 +1,3 @@
 import {getCookie} from "https://jscroot.github.io/cookie/croot.js";
+export let UrlLogin = "https://asia-southeast2-argon-fire-401902.cloudfunctions.net/loginGISChapter5"
 export let UrlRegister = "https://asia-southeast2-argon-fire-401902.cloudfunctions.net/regisGISChpater5"
