132c326b3957643951ac1f98c88ed59adb606212
Raff <rafiazhim.ra@gmail.com>
2023-11-27 15:22:35 +0700

    update(func.js): update add response login

--- assets/js/controller/func.js
+++ assets/js/controller/func.js
@@ -12,3 +12,38 @@ export function getTokenFromAPI() {
       })
       .catch(error => console.error('Gagal mengambil token:', error));
   }
+
+export function ResponsePostLogin(response) {
+    if (response && response.token) {
+      console.log('Token User:', response.token);
+      setCookieWithExpireHour('Login', response.token, 3);
+      window.location.href = 'https://1214005gis5.my.id/dashboard';
+      alert("Selamat Datang")
+    } else {
+      alert('Login gagal. Silakan coba lagi.');
+    }
+  }
+
+export function ResponseLogin(result) {
+    ResponsePostLogin(result)
+  }
+
+export function PostLogin() {
+    const username = getValue("username");
+    const password = getValue("password");
+  
+    const data = {
+      username: username,
+      password: password
+    };
+    return data;
+  }
+
+export function AlertPost(value){
+    alert(value.message + "\nRegistrasi Berhasil")
+    window.location.href= "signin.html"
+}
+
+export function ResponsePost(result) {
+    AlertPost(result);
+}
