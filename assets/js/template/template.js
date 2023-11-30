6dffd2011afcd70b9dd869f1aa7e0b867808e372
Raff <rafiazhim.ra@gmail.com>
2023-11-30 09:30:06 +0700

    update(template.js): rename mainlink.js

---
+++ assets/js/template/template.js
@@ -0,0 +1,3 @@
+export let UrlRegister = `https://asia-southeast2-argon-fire-401902.cloudfunctions.net/postRegisterEDUMAS`
+export let UrlLogin = `https://asia-southeast2-argon-fire-401902.cloudfunctions.net/postLoginEDUMAS`
+export let token = 'token';
