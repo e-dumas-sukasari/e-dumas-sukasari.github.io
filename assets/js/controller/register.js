59deb5915d8fb59457e4d2abddb0f3badaae315f
Raff <rafiazhim.ra@gmail.com>
2023-11-30 09:33:18 +0700

    update(register.js): moved to controller

---
+++ assets/js/controller/register.js
@@ -0,0 +1,14 @@
+import { postWithBearer } from "https://jscroot.github.io/api/croot.js";
+import {GetDataForm,  ResponsePost} from "../config/config.js";
+import { token, URLPost } from "../template/template.js";
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.querySelector("regis-form");
+
+    form.addEventListener("submit", function(event) {
+        event.preventDefault();
+        let data = GetDataForm();
+        postWithBearer(URLPost, token, data, ResponsePost)
+    });
+});
