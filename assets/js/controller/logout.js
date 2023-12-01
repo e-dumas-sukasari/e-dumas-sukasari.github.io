0c703e594ca2102845accb84184b404cfc09f295
Raff <rafiazhim.ra@gmail.com>
2023-12-01 20:06:42 +0700

    update(logout): fixed

--- assets/js/controller/logout.js
+++ assets/js/controller/logout.js
@@ -1,10 +1,8 @@
 import { deleteCookie } from 'https://jscroot.github.io/cookie/croot.js';
 
 function logout() {
-
   deleteCookie('Login');
-  window.location.href = 'https://e-dumas-sukasari.github.io';
+  window.location.href = '/';
 }
-
 
 document.getElementById('logoutButton').addEventListener('click', logout);
